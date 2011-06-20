8b317f8bff4b2354358d134a08ddec2201647124
John Tjanaka <zonecheung@gmail.com>
2011-06-20 07:52:58 -0500

    Fix the railmail saving array.

--- lib/railmail/observer.rb
+++ lib/railmail/observer.rb
@@ -1,8 +1,8 @@
 module Railmail
   class Observer
     def self.delivered_email(mail)
-      ::RailmailDelivery.create!(:recipients => mail.to,
-                                 :from       => mail.from,
+      ::RailmailDelivery.create!(:recipients => mail.to.to_a,
+                                 :from       => mail.from.to_a,
                                  :subject    => mail.subject,
                                  :sent_at    => Time.now,
                                  :raw        => mail)
