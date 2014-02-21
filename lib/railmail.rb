d83cc4bae62e4b1344c45f1b50f98219d3881c55
Shawn Balestracci <shawn@vegantech.com>
2014-02-21 11:57:27 -0800

    move assets into railmail

--- lib/railmail.rb
+++ lib/railmail.rb
@@ -1,8 +1,15 @@
 require 'will_paginate' 
 require 'railmail/observer'
+require 'sprockets/railtie'
 
 module Railmail
   class Engine < Rails::Engine
+    engine_name 'railmail'
+    initializer :assets do |app|
+      Rails.application.config.assets.precompile += %w(railmail.js)
+    end
+
+
   end if defined?(Rails)
 end
 
